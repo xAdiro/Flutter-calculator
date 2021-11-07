0f95560dd97ae60e6f4e09c52f797a9e28be7746
Adrian <adiro.rostek@gmail.com>
2021-11-07 02:44:03 +0100

    Creating the base of future layout

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:calculator/button_panel.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: ButtonPanel(),
+        ),
+      ),
+    );
+  }
+}
